5c799b03a53b8dd20360e18bbf2f3becd4ab0db2
Saurav2411 <saurich1999@gmail.com>
2025-04-05 07:05:21 +0530

    link

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -37,7 +37,7 @@ export const About = () => {
             <div className="flex justify-center mt-6 space-x-4">
             
               <a
-                href="https://drive.google.com/file/d/1S7Hnn8MNHL13gsKuJl3f36raC7hxpxr3/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1S7Hnn8MNHL13gsKuJl3f36raC7hxpxr3/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="bg-blue-500 text-white py-3 px-6 rounded-lg font-medium transition hover:-translate-y-0.5 hover:shadow-lg"
